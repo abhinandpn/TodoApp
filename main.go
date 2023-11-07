16a65ce792b46445a6d45eedd9c95a5a97ef866f
abhinandpn <abhinandpn4067@gmail.com>
2023-11-07 10:14:49 +0530

    finalizing the sample project

--- main.go
+++ main.go
@@ -1,38 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 
 	"github.com/abhinandpn/TodoApp/Todo"
 )
 
-var Tmpl *template.Template
+var Tmp *template.Template
 
 func main() {
-
 	mux := http.NewServeMux()
 
 	// Check if the templates directory exists
-	if _, err := os.Stat("templetes"); os.IsNotExist(err) {
+	if _, err := os.Stat("templates"); os.IsNotExist(err) {
 		log.Fatal("Templates directory does not exist")
 	}
 
 	// List files in the templates directory
-	files, err := filepath.Glob("templetes/*.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("List of HTML files in templates directory:")
-	for _, file := range files {
-		fmt.Println(file)
-	}
-
-	Tmpl = template.Must(template.ParseGlob("templetes/*.html"))
+	Todo.FilePass()
+	fs := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 	mux.HandleFunc("/todo", Todo.Todo)
-	log.Fatal(http.ListenAndServe(":9090", mux))
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
